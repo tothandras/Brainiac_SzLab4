c07d27ec30bbd4c584590f6a51a705bbe3ceda80
tothandras <andras.toth93@gmail.com>
2014-04-12 12:36:39 +0200

    GameEngine alakulgat

---
+++ src/main/java/com/brainiac/controller/GameState.java
@@ -0,0 +1,11 @@
+package com.brainiac.controller;
+
+/**
+ * Project name: Brainiac_SzLab4
+ * User: tothandras
+ * Date: 2014.04.12.
+ * Time: 11:52
+ */
+public enum GameState {
+    Build, Step
+}
