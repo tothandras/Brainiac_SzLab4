5a76364675f8ac5fbd8b00f6d73a632f040e877f
tothandras <andras.toth93@gmail.com>
2014-04-12 11:18:48 +0200

    GameEngine alakul

--- src/main/java/com/brainiac/model/Map.java
+++ src/main/java/com/brainiac/model/Map.java
@@ -1,7 +1,7 @@
 package com.brainiac.model;
 
-import com.brainiac.Skeleton;
-
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
@@ -12,13 +12,30 @@ public class Map {
     public Map(int width, int height) {
         this.width = width;
         this.height = height;
-        Skeleton.writeFunctionDetails("Map(int: "+width+", int: "+height+")");
-        Skeleton.writeReturnValue("");
+        paths = new ArrayList<Path>();
+        Path path = new Path();
+        Line2D road1 = new Line2D.Double(0, height / 2.0, width / 3.0, height / 2.0);
+        Line2D road2 = new Line2D.Double(width / 3.0, height / 3.0, 2.0 * width / 3.0, 2.0 * height / 3.0);
+        Line2D road3 = new Line2D.Double(width / 3.0, 2.0 * height / 3.0, 2.0 * width / 3.0, 2.0 * height / 3.0);
+        Line2D road4 = new Line2D.Double(2.0 * width / 3.0, 2.0 * height / 3.0, 2.0 * width / 3.0, height / 2.0);
+        Line2D road5 = new Line2D.Double(2.0 * width / 3.0, height / 2.0, width, height / 2.0);
+        path.roads.add(road1);
+        path.roads.add(road2);
+        path.roads.add(road3);
+        path.roads.add(road4);
+        path.roads.add(road5);
+        paths.add(path);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public List<Path> getPaths() {
-        Skeleton.writeFunctionDetails("Map.getPaths()");
-        Skeleton.writeReturnValue("paths: List<Path>");
         return paths;
     }
 }
--- src/main/java/com/brainiac/model/Path.java
+++ src/main/java/com/brainiac/model/Path.java
@@ -1,16 +1,13 @@
 package com.brainiac.model;
 
-import com.brainiac.Skeleton;
-
 import java.awt.geom.Line2D;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Path {
-    // atneveztem a valtozot
     public List<Line2D> roads;
 
     public Path() {
-        Skeleton.writeFunctionDetails("Path()");
-        Skeleton.writeReturnValue("");
+        roads = new ArrayList<Line2D>();
     }
 }
